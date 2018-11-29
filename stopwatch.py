5d49c53a115a0ed3fdba34faf90387a42e01569d
John Lin <jlin1674@gmail.com>
2018-11-29 00:25:10 -0500

    stopwatch.py

--- stopwatch.py
+++ stopwatch.py
@@ -1,18 +1,32 @@
 import time
+''' Using functions time and sleep (for testing) '''
 
 class Stopwatch:
-    def __init__():
+    def __init__(self):
         ''' Sets time elapsed to 0'''
         self.elapsed = 0
 
-    def start():
+    def start(self):
         ''' Starts the timer '''
         self.start = time.time()
 
-    def end():
+    def end(self):
         ''' Ends the timer and adds time to elapsed '''
         # in case someone calls end twice
         if self.start:
             self.elapsed += time.time() - self.start
             self.start = False
 
+
+if __name__ == '__main__':
+    sw = Stopwatch()
+    
+    # testing passing 1 second of time
+    sw.start()
+    time.sleep(1)
+    sw.end()
+    print(sw.elapsed)
+
+    # testing that calling end does nothing
+    sw.end()
+    print(sw.elapsed)
