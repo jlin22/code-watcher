74cfc1e067e1e66cf4faf68e7c5afdec97eef34b
John Lin <jlin1674@gmail.com>
2018-12-01 20:35:46 -0500

    version 1

--- table.py
+++ table.py
@@ -1,3 +1,17 @@
 class Table:
     ''' Table interacts with csv files ''' 
+    ''' Design Choice: Use a list of lists '''
+
+    def read_csv(self, f):
+        with open(f) as file_object:
+            self.data = file_object.readlines()
+            for i in range(len(self.data)):
+                self.data[i] = self.data[i].strip('\n').split(', ') 
+        return self.data
+
+if __name__ == '__main__':
+    t = Table()
+    print(t.read_csv('progress.csv'))
+
+
 
