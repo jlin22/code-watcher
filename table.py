869a20ccc200cf3c8d30c29a2c12970e1c39c3bb
John Lin <jlin1674@gmail.com>
2018-12-03 23:09:12 -0500

    time_spent

--- table.py
+++ table.py
@@ -24,19 +24,38 @@ class Table:
                         self.content['-'.join(data[i][:3])] = [data[i][3:]]
             print('-'.join(data[1][:3])) 
         return self.content
+    
+    def time_spent(self, date, entry):
+
+        # calculate the differences between the intervals of time
+        # [year, month, day, hour, minute, day]
+        list_date = date.split('-')
+        differences = []
+        for i in range(3, 6):
+            print(i - 3)
+            differences.append(int(entry[i]) - int(list_date[i - 3]))
+        for i in range(0, 3):
+            differences.append(int(entry[i + 6]) - int(entry[i]))
+        return differences
 
     def search_day(self, date):
         ''' Searches for all entries with date
         date is of the form: 'yyyy-mm-dd' '''
-        try:
-            return self.content[date]
+        print(self.content[date])
+        print(date)
+        print(self.time_spent(date, self.content[date][0]))
+        '''
         except:
             print('That date is not in the data')
+            '''
+
+
 
 if __name__ == '__main__':
     t = Table()
     print(t.read_csv('progress.csv'))
     print(t.search_day('2018-12-01'))
+    
 
 
 
